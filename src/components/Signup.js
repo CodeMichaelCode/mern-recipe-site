acfe93aee7583573b8902c9d715c03d4e598519a
Michael Olawunmi <molawunmi21@gmail.com>
2022-07-11 10:32:40 -0400

    fixed signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,49 @@
-import React from "react";
+import axios from "axios";
 import StyledAuth from "./styles/Auth.styled";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: String(e.target.value),
+    }));
+  };
+
+  const sendRequest = async () => {
+    const res = await axios.post(
+      "https://node-recipebookmark.herokuapp.com/signup",
+      {
+        name: inputs.name,
+        email: inputs.email,
+        password: inputs.password,
+      }
+    );
+    const data = await res.data;
+    return data;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest().then(() => navigate("/"));
+  };
   return (
-    <StyledAuth>
+    <StyledAuth onSubmit={handleSubmit}>
       <h2>Signup</h2>
       <label htmlFor="fullname">Full Name</label>
-      <input type="text" name="fullname" required />
+      <input type="text" name="fullname" required onChange={handleChange} />
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" required />
+      <input type="email" name="email" required onChange={handleChange} />
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" required />
+      <input type="password" name="password" required onChange={handleChange} />
       <input type="submit" value="Submit" />
     </StyledAuth>
   );
---
+++ src/components/teest.js
@@ -0,0 +1,18 @@
+// var thing;
+
+// let func = (str = test) => {
+//   console.log(str);
+// };
+
+// func(thing);
+// func("hey");
+
+// const a = function b() {
+//   console.log("b");
+// };
+
+// console.log(typeof a);
+
+for (var i = 0; i < 4; i++) {
+  setTimeout(() => console.log(i), 100);
+}
